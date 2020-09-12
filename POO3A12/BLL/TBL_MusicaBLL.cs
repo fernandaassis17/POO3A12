3cf574e837a1e646402b0fc1d7c9197b150a224b
Fernanda Marques Assis <65399666+fernandaassis17@users.noreply.github.com>
2020-09-12 20:57:44 -0300

    Add files via upload

---
+++ POO3A12/BLL/TBL_MusicaBLL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using POO3A12.DAL;
+using POO3A12.DTO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace POO3A12.BLL
+{
+    class TBL_MusicaBLL : TBL_MusicaDTO
+    {
+
+        private GravadoraBD daoBanco = new GravadoraBD();
+
+        public DataTable ListarMusica()
+        {
+            string sql = string.Format($@"select * from TBL_Musica");
+            return daoBanco.ExecutarConsulta(sql);
+        }
+
+        public DataTable PesquisarMusica(string condicao)
+        {
+            string sql = string.Format($@"select * from TBL_Musica where " + condicao);
+            return daoBanco.ExecutarConsulta(sql);
+        }
+        public DataTable PesquisarMusica()
+        {
+            string sql = string.Format($@"select * from TBL_Musica");
+            return daoBanco.ExecutarConsulta(sql);
+        }
+
+        public void AlterarMusica(TBL_MusicaBLL DtoMusica)
+        {
+            string sql = string.Format($@"UPDATE TBL_Musica set idMusica = '{DtoMusica.IdMusica}',
+                                                                 idCD = '{DtoMusica.IdCD}',
+                                                                 idGravadora = '{DtoMusica.IdGravadora}',
+                                                                 nomeAutor = '{DtoMusica.NomeAutor}'
+                                                   where nome = '{DtoMusica.Nome}';");
+            daoBanco.ExecutarComando(sql);
+        }
+
+        public void InserirMusica(TBL_MusicaBLL ObjMusica)
+        {
+            string sql = string.Format($@"INSERT INTO TBL_Musica VALUES (NULL, '{ObjMusica.IdMusica}',
+                                                                                '{ObjMusica.IdCD}',
+                                                                                '{ObjMusica.IdGravadora}',
+                                                                                '{ObjMusica.NomeAutor}',
+                                                                                '{ObjMusica.Nome}');");
+            daoBanco.ExecutarComando(sql);
+        }
+
+
+        // Metodo utilizado para excluir Cliente no Banco
+        public void ExcluirMusica(TBL_MusicaBLL objMusica)
+        {
+            string sql = string.Format($@"DELETE FROM TBL_Musica where idMusica = {objMusica.IdMusica};");
+            daoBanco.ExecutarComando(sql);
+        }
+
+    }
+}
---
+++ POO3A12/DTO/TBL_MusicaDTO.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using POO3A12.DAL;
+using POO3A12.BLL;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO3A12.DTO
+{
+    class TBL_MusicaDTO
+    {
+
+        private int idMusica, idCD, idGravadora;
+        private string nome, nomeAutor;
+
+        public int IdMusica { get => idMusica; set => idMusica = value; }
+        public string NomeAutor { get => nomeAutor; set => nomeAutor = value; }
+
+
+        public int IdCD
+        {
+            set
+            {
+                if (value !=  0)
+                {
+                    this.idCD = value;
+                }
+                else
+                {
+                    throw new Exception("O campo id CD é obrigatório!!");
+                }
+
+            }
+            get { return this.idCD; }
+        }
+
+        public int IdGravadora
+        {
+            set
+            {
+                if (value !=  0)
+                {
+                    this.idGravadora = value;
+                }
+                else
+                {
+                    throw new Exception("O campo id Gravadora é obrigatório!!");
+                }
+
+            }
+            get { return this.idGravadora; }
+        }
+
+        public string Nome
+        {
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.nome = value;
+                }
+                else
+                {
+                    throw new Exception("O campo nome é obrigatório!!");
+                }
+
+            }
+            get { return this.nome; }
+        }
+    }
+}
